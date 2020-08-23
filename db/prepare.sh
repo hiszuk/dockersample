04429244ced5833a6daafd15349f9fce033d8a4f
Hisakazu KATO <hisakazu.katou@daikin.co.jp>
2020-08-24 00:53:33 +0900

    初期登録

---
+++ db/prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+# $MYSQL_DATABASE用のユーザーと権限の作成
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_APP_USER'@'%' IDENTIFIED BY '$MYSQL_APP_PASSWORD';"
+
